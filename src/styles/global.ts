c6e665f08f8bf9f5bd00c5e60ef7e4319d0a830e
IsraelSantos7792 <israelqueren1805@gmail.com>
2021-12-09 14:56:59 -0300

    ModalTransaction

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -53,4 +53,23 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 600;
   }
   
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+
+  }
+
+
 `
